7261ca24d2cbabaa4d7b5209086e11b2e373a64a
Kiran <itskkirran@gmail.com>
2026-04-01 14:42:44 +0530

    login-register-pages

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,8 @@
+export default function Register() {
+    return (
+      <div>
+        <h1>Hello Register</h1>
+        <p>heyyyyy</p>
+      </div> 
+    );
+  }
---
+++ app/profile/[profileid]/page.tsx
@@ -0,0 +1,13 @@
+export default async function Profile({params}) {
+    const { profileid } = await params;
+
+    
+
+    return (
+        <div>Profile says hello {profileid}
+        
+        </div>
+
+        
+    )
+}
